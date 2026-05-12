8489290b782bbe638a19b83f9aeda82745a8ed05
narsinggurme <narsinggurme@gmail.com>
2026-05-12 12:26:42 +0530

    Ransom note problem

---
+++ RansomNote.java
@@ -0,0 +1,34 @@
+package com.may01;
+
+public class RansomNote 
+{
+	public static boolean ransomNote(String s, String t)
+	{
+		int[] count = new int[26];
+		for(char ch : t.toCharArray())
+		{
+			count[ch -'a']++;
+		}
+		
+		for(char ch : s.toCharArray())
+		{
+			count[ch - 'a']--;
+			
+			if(count[ch-'a'] < 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args)
+	{
+		String ransomeNote = "bg";
+		String magazine = "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj";
+		
+		System.out.println(ransomNote(ransomeNote, magazine));
+		
+	}
+
+}
