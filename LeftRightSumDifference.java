d7cddb76b409d8becc7b65a99df153610b949c83
narsinggurme <narsinggurme@gmail.com>
2026-06-06 20:33:57 +0530

    Left and right sum difference

---
+++ LeftRightSumDifference.java
@@ -0,0 +1,35 @@
+package com.may01;
+
+import java.util.Arrays;
+
+public class LeftRightSumDifference 
+{
+	public static int[] leftRightDiff(int[] nums)
+	{
+		int n = nums.length;
+		int[] ans = new int[n];
+		int totalSum = 0;
+		for(int num : nums)
+		{
+			totalSum += num;
+		}
+		
+		int leftSum = 0;
+		for(int i=0; i<n; i++)
+		{
+			int rightSum = totalSum - leftSum - nums[i];
+			ans[i] = Math.abs(leftSum -  rightSum);
+//			ans[i] = Math.abs(rightSum -  leftSum);
+			leftSum += nums[i];
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) 
+	{
+		int[] arr = {10,4,8,3};
+
+		System.out.println(Arrays.toString(leftRightDiff(arr)));
+	}
+
+}
