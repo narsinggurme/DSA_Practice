f1bd8649dc30b9499903f109c0587956a00d48f3
narsinggurme <narsinggurme@gmail.com>
2026-06-12 22:13:07 +0530

    find kth positive brutforce Approch

--- FindKthPositive.java
+++ FindKthPositive.java
@@ -4,42 +4,42 @@ public class FindKthPositive
 {
 	public static int findKthPositive(int[] arr, int k)
 	{
-		int left = 0;
-		int right = arr.length -1;
-		while(left <= right)
-		{
-			int mid = left + (right - left) /2;
-			int missing = arr[mid] - (mid+1);
-			System.out.println("missing: " + missing + "| mid: " + mid);
-			
-			if(missing < k)
-			{
-				left =mid +1;
-			}
-			else
-			{
-				right = mid -1;
-			}
-		}
-		return left + k;
-//		int j = 0;
-//		int num = 1;
-//		while(true)
+//		int left = 0;
+//		int right = arr.length -1;
+//		while(left <= right)
 //		{
-//			if(j <= arr.length && arr[j] == num)
+//			int mid = left + (right - left) /2;
+//			int missing = arr[mid] - (mid+1);
+//			System.out.println("missing: " + missing + "| mid: " + mid);
+//			
+//			if(missing < k)
 //			{
-//				j++;
+//				left =mid +1;
 //			}
 //			else
 //			{
-//				k--;
-//				if(k==0)
-//				{
-//					return num;
-//				}
+//				right = mid -1;
 //			}
-//			num++;
 //		}
+//		return left + k;
+		int j = 0;
+		int num = 1;
+		while(true)
+		{
+			if(j <= arr.length && arr[j] == num)
+			{
+				j++;
+			}
+			else
+			{
+				k--;
+				if(k==0)
+				{
+					return num;
+				}
+			}
+			num++;
+		}
 		
 	}
 
