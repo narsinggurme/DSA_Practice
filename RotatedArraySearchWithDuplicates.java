6add3c06136bd838112b90be1bd84d1035255250
narsinggurme <narsinggurme@gmail.com>
2026-06-14 14:29:59 +0530

    rotated arrays search with duplicates

---
+++ RotatedArraySearchWithDuplicates.java
@@ -0,0 +1,59 @@
+package com.may01;
+
+public class RotatedArraySearchWithDuplicates {
+	public static boolean rotatedArraySearchWithDuplicates(int[] nums, int target) 
+	{
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left <= right) 
+		{
+			int mid = left + (right - left) / 2;
+			
+			if(nums[mid] == target)
+			{
+				return true;
+			}
+			if(nums[left] == nums[mid] && nums[mid] == nums[right])
+			{
+				right--;
+				left++;
+			}
+			else if(nums[left] <= nums[mid])
+			{
+				if(target >= nums[left] && target < nums[mid])
+				{
+					right = mid - 1;
+				}
+				else
+				{
+					left = mid + 1;
+				}
+				
+			}
+			else
+			{
+				if(target > nums[mid] && target <= nums[right])
+				{
+					left = mid + 1;
+				}
+				else
+				{
+					right = mid -1;
+				}
+			}
+				
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args)
+	{
+		int[] nums = { 2, 5, 6, 0, 0, 1, 2 };
+		int target = 3;
+
+		System.out.println(rotatedArraySearchWithDuplicates(nums, target));
+	}
+
+}
